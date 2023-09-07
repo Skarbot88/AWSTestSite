1bc707d49e1ca5a222d25095f0d078bc249df922
Sundhar Karunanidi <50683064+Skarbot88@users.noreply.github.com>
2023-09-07 15:51:33 +0100

    Update config.js
    wrong region added

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'st-1_6K9CDfqIg', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7tc9ah0u951gcouh3vfit625uk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'eu-west-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
