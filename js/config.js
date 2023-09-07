6114d7c4a36dfa302e82f45294bce8db8de6830f
Sundhar Karunanidi <50683064+Skarbot88@users.noreply.github.com>
2023-09-07 15:44:52 +0100

    Update config.js
    Added user pool for cognito, config changes

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'st-1_6K9CDfqIg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7tc9ah0u951gcouh3vfit625uk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'eu-west-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
